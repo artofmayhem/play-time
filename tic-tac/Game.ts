6601889e9689bfe01330d5fc987927ba0a1bbdb7
John C. Kennedy <devjckennedy@gmail.com>
2020-08-02 18:15:54 +0900

    check for winner and valid move

--- tic-tac/Game.ts
+++ tic-tac/Game.ts
@@ -1,13 +1,16 @@
 import { Board } from "./Board"
 import { Player } from "./Player"
+import { tag } from "./utils"
+import { WINNING_INDICES } from "./constants"
 
 export class Game {
   private p1!: Player
   private p2!: Player
   private count: number
+  private hasWinner = false
   private readonly gameBoard: Board
   private players: Record<string, string> = {}
-  private tags: Readonly<Array<string>> = ["✘", "〇"]
+  private tags: Readonly<Array<string>> = ["✘", "◯"]
 
   constructor() {
     this.count = 0
@@ -25,6 +28,16 @@ export class Game {
     this.players[this.p2.name] = this.tags[1 - index]
   }
 
+  checkWinner() {
+    this.hasWinner = WINNING_INDICES.some((indices) =>
+      indices.every((index) => this.gameBoard.board[index] === this.tag)
+    )
+
+    if (this.hasWinner) {
+      console.log(`\x1b[92m${this.player} WON this round.\x1b[0m`)
+    }
+  }
+
   get player() {
     return this.count % 2 == 0 ? this.p1.name : this.p2.name
   }
@@ -33,42 +46,58 @@ export class Game {
     return this.players[this.player]
   }
 
+  isValidMove(index: number) {
+    return this.gameBoard.board[index] == null
+  }
+
   play(input: number) {
-    this.gameBoard.board[input - 1] = this.tag
+    if (this.hasWinner) {
+      return
+    }
+
+    const index = --input
+    if (!this.isValidMove(index)) {
+      console.error(
+        `\x1B[31m${this.player} made an invalid move. Move was already played, please try again.\x1B[0m`
+      )
+      return
+    }
+
+    this.gameBoard.board[index] = this.tag
+    this.checkWinner()
+    this.showBoard()
     this.count++
   }
 
   showBoard() {
     const { board } = this.gameBoard
+    const title = `
+_____________
+Game  Board
+=============
+`
+    let content = "\x1B[34m"
+    content += title + "\n"
 
-    console.log(
-      `
-        _____________
-         Game  Board
-        =============
+    Array.from({ length: 9 }, (_, i) => i).forEach((_, i) => {
+      content += i == 0 ? "-------------\n" : ""
+      content += `| ${tag(board[i])} \x1B[34m`
+      content += (i + 1) % 3 == 0 ? "|\n-------------\n" : ""
+      content += i == 8 ? "\x1B[0m" : ""
+    })
 
-        -------------
-        | ${board[0] ?? " "} | ${board[1] ?? " "} | ${board[2] ?? " "} |
-        -------------
-        | ${board[3] ?? " "} | ${board[4] ?? " "} | ${board[5] ?? " "} |
-        -------------
-        | ${board[6] ?? " "} | ${board[7] ?? " "} | ${board[8] ?? " "} |
-        -------------
-      `
-    )
+    console.log(content)
   }
 
   showInfo() {
     const { p1, p2 } = this
 
     console.info(
-      `
-        _____________
-        Players Info
-        =============
-        ${p1?.name} - ${this.players[p1?.name]}
-        ${p2?.name} - ${this.players[p2?.name]}
-      `
+      `______________
+Players' Info
+==============
+${p1?.name} - ${tag(this.players[p1?.name])}
+${p2?.name} - ${tag(this.players[p2?.name])}`
     )
   }
 
@@ -76,5 +105,6 @@ export class Game {
     this.addPlayers()
     this.assignTags()
     this.showInfo()
+    this.showBoard()
   }
 }
