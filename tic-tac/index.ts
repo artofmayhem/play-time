6193360df2c4f8eb8700bb4b5550578109c93792
John C. Kennedy <devjckennedy@gmail.com>
2020-08-03 03:21:52 +0900

    add support for user input

--- tic-tac/index.ts
+++ tic-tac/index.ts
@@ -1,15 +1,36 @@
+import * as readline from "readline"
 import { Game } from "./Game"
 
 const game = new Game()
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
 game.start()
-// Player 1
-game.play(7)
-// Player 2 - invalid move
-game.play(7)
-// Player 2 - valid move
-game.play(1)
-game.play(8)
-game.play(2)
-game.play(9)
-game.play(3)
+
+function play() {
+  rl.question(
+    `${game.player}, please enter a number between 1 - 9, to play: `,
+    (answer) => {
+      const move = parseInt(answer, 10)
+      const isNaN = Number.isNaN(Number(move))
+
+      if (isNaN || move < 1 || move > 9) {
+        console.log("\x1B[31mInvalid number entered, please try again.\x1B[30m")
+        play()
+        return
+      }
+
+      game.play(move)
+
+      if (game.isDraw || game.hasWinner) {
+        rl.close()
+      } else {
+        play()
+      }
+    }
+  )
+}
+
+play()
