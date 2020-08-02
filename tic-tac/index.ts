74a5b02951b18be750f03cd0f9356008d8b45201
John C. Kennedy <devjckennedy@gmail.com>
2020-08-02 18:16:10 +0900

    update tester

--- tic-tac/index.ts
+++ tic-tac/index.ts
@@ -3,4 +3,13 @@ import { Game } from "./Game"
 const game = new Game()
 
 game.start()
-game.showBoard()
+// Player 1
+game.play(7)
+// Player 2 - invalid move
+game.play(7)
+// Player 2 - valid move
+game.play(1)
+game.play(8)
+game.play(2)
+game.play(9)
+game.play(3)
